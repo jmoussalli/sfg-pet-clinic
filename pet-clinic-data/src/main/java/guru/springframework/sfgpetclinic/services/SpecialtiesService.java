00791baa1b6ded9d0ee58b1d0f6f493a3afea032
jmoussalli <jerome.moussalli@gmail.com>
2021-01-11 18:11:56 +0100

    Create Specialty Map Service Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+
+/**
+ * Package :  guru.springframework.sfgpetclinic.services
+ * Created by jerom on 11/01/2021 at 16:32
+ */
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,40 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import guru.springframework.sfgpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Package :  guru.springframework.sfgpetclinic.services.map
+ * Created by jerom on 11/01/2021 at 16:34
+ */
+@Service
+public class SpecialityMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
