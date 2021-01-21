c9c3bb3b501d2c6aa2a80314aafe54d79aa51dd0
jmoussalli <jerome.moussalli@gmail.com>
2021-01-21 10:39:31 +0100

    Create Spring Data JPA Service for Vets. Closes #41.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VetRepository.java
@@ -1,10 +1,12 @@
 package guru.springframework.sfgpetclinic.repositories;
 
 import guru.springframework.sfgpetclinic.model.Vet;
-import guru.springframework.sfgpetclinic.services.CrudService;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by jerom on 20/01/2021 at 17:41
  */
-public interface VetRepository extends CrudService<Vet, Long> {
+@Repository
+public interface VetRepository extends CrudRepository<Vet, Long> {
 }
